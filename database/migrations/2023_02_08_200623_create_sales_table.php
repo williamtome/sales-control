85f0e7a87c2a245b1a65f10917869b1932118ab4
William Tome <william.tomenh@gmail.com>
2023-02-09 10:29:48 -0300

    initial olw project

---
+++ database/migrations/2023_02_08_200623_create_sales_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained();
+            $table->foreignId('seller_id')->constrained();
+            $table->timestamp('sold_at');
+            $table->integer('total_amount');
+            $table->char('status', 1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+};
