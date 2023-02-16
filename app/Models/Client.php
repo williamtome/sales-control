343a317a1dc652f715c628b092da32f7964f6172
William Tome <william.tomenh@gmail.com>
2023-02-15 21:15:10 -0300

    feat: create clients crud.

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Client extends Model
@@ -18,9 +17,9 @@ class Client extends Model
         'user_id',
     ];
 
-    public function address(): HasOne
+    public function address(): BelongsTo
     {
-        return $this->hasOne(Address::class);
+        return $this->belongsTo(Address::class);
     }
 
     public function user(): BelongsTo
---
+++ resources/views/components/select-address.blade.php
@@ -0,0 +1,8 @@
+@props(['disabled' => null, 'seletedValue' => null])
+
+<select name="address_id" id="address_id" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500
+dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
+    @foreach (App\Models\Address::all() as $option)
+        <option {{ isset($selectedValue) && $option->id == $selectedValue ? 'selected' : '' }} value="{{ $option->id }}">{{ $option->city }} - {{ $option->state }}</option>
+    @endforeach
+</select>
