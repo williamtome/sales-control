4047951f822c4bac3acaa74c7b97942676f657d7
William Tome <william.tomenh@gmail.com>
2023-02-20 19:35:32 -0300

    feat: use OpenAI package to generate data structure for Vega Lite.

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
+use App\Models\SalesCommission;
 use Illuminate\Support\Facades\Route;
+use OpenAI\Laravel\Facades\OpenAI;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,6 +31,20 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('/clients', ClientController::class);
+
+    Route::get('/chart', function () {
+        $fields = implode(', ', SalesCommission::getColumns());
+
+        $question = 'Gere um gráfico das vendas por empresa no eixo y ao longo dos últimos 5 anos';
+
+        $config = OpenAI::completions()->create([
+            'model' => 'text-davinci-003',
+            'prompt' => "Considerando a lista de campos ($fields), gere uma configuração json do Vega Lite v5 (sem campo de dados e com descrição) que atenda o seguinte pedido $question. Resposta:",
+            'max_tokens' => 1500
+        ])->choices[0]->text;
+
+        dd($config);
+    });
 });
 
 require __DIR__.'/auth.php';
